4b36af3ccabb2a5a194afa8b6532d6da2482accc
Fuususu <112471253+Fuususu@users.noreply.github.com>
2024-11-25 12:22:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,7 +27,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCIqJ6sLQd7lR3ib9pPT3wr_LHQE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'EkWs798QZUKncXprrRhVsrjjFnRC1iPq7bMadu5RTug	',
+      useTemplateId: 'kVreXJjvdAzJwRYyXg3HvaMYCA6ET2EH2YmzalmlOuU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-22',
       festivals: [
@@ -56,7 +56,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCIqJ6pISvIoTThAWoqch66nR2nU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'EkWs798QZUKncXprrRhVsrjjFnRC1iPq7bMadu5RTug	',
+      useTemplateId: 'kVreXJjvdAzJwRYyXg3HvaMYCA6ET2EH2YmzalmlOuU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-22',
       festivals: [
